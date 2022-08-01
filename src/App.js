86f877a8ff4e35a0f9caf7b59be4019df7c73a8e
ice949 <justineimasiku21@gmail.com>
2022-08-01 22:36:44 +0200

    Update README file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to Math Magicians</h2>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to My Math Magicians app</h2>
+      </div>
+      <p className="App-intro">
+        Development In progress and will be available for use soon #Ice-berg
+      </p>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
