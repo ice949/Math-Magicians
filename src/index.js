d8e72e31b6348eceaf43b22699a53adde59673bd
ice949 <justineimasiku21@gmail.com>
2022-08-02 13:25:40 +0200

    Create UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calculator from './components/Calculator';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Calculator />,
   document.getElementById('root'),
 );
